2ede69bff0213832c23122475d650302133a9647
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-09-02 10:44:13 -0300

    first commit

---
+++ consultoria_presencial.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Plugin Name: Consultoria presencial
+* Plugin URI: https://github.com/lucassdantas/consultoria_presencial_plugin_wp.git
+* Description: Consultoria presencial
+* Version: 0.1
+* Author: Lucas Dantas
+* Author URI: lucassdantas.github.io
+**/
+
+defined('ABSPATH') or die();
+if(!function_exists('add_action')){
+    die;
+}
