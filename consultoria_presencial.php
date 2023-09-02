c4850934b7010952268fb6e584e801ffbcf6f556
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-09-02 19:39:51 -0300

    add tab to my account

--- consultoria_presencial.php
+++ consultoria_presencial.php
@@ -12,3 +12,40 @@ defined('ABSPATH') or die();
 if(!function_exists('add_action')){
     die;
 }
+
+function consultoria_presencial_endpoint() {
+    add_rewrite_endpoint( 'premium-support', EP_ROOT | EP_PAGES );
+}
+  
+add_action( 'init', 'consultoria_presencial_endpoint' );
+  
+// ------------------
+// 2. Add new query var
+  
+function consultoria_presencial( $vars ) {
+    $vars[] = 'consultoria-presencial';
+    return $vars;
+}
+  
+add_filter( 'query_vars', 'consultoria_presencial', 0 );
+  
+// ------------------
+// 3. Insert the new endpoint into the My Account menu
+  
+function add_consultoria_presencial_tab( $items ) {
+    $items['consultoria-presencial'] = 'Consultoria Presencial';
+    return $items;
+}
+  
+add_filter( 'woocommerce_account_menu_items', 'add_consultoria_presencial_tab' );
+  
+// ------------------
+// 4. Add content to the new tab
+  
+function conteudo_consultoria_presencial() {
+    echo '<h3>Sua consultoria presencial</h3>';
+    #echo do_shortcode( ' /* your shortcode here */ ' );
+}
+  
+add_action( 'woocommerce_account_premium-support_endpoint', 'conteudo_consultoria_presencial' );
+// Note: add_action must follow 'woocommerce_account_{your-endpoint-slug}_endpoint' format
