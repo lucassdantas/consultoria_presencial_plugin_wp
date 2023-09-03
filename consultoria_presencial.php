09aefc1d935be45cefaa636c948cc68d57e83bbc
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-09-02 21:50:05 -0300

    testing payment successfuly hook

--- consultoria_presencial.php
+++ consultoria_presencial.php
@@ -14,3 +14,37 @@ if(!function_exists('add_action')){
 }
 
 require_once plugin_dir_path( __FILE__ ). 'src/add_my_account_tab.php';
+
+add_action('woocommerce_before_checkout_form', 'update_user_id');
+
+function update_user_id(){
+    $user_id = get_current_user_id(); 
+    $meta_key = 'agendamentos-presenciais-disponiveis'; 
+    
+    $booking_quantity = 2;
+    
+    $old_meta_value = get_user_meta($user_id, $meta_key, true);
+    $new_meta_value = strval(intval($old_meta_value) + $booking_quantity);
+    update_user_meta($user_id, $meta_key, $new_meta_value);
+}
+
+// Hook into the 'woocommerce_payment_complete' action.
+add_action('woocommerce_payment_complete', 'check_payment_status');
+
+function check_payment_status($order_id) {
+    // Get the order object
+    $order = wc_get_order($order_id);
+    echo '<pre>';
+    print_r($order);
+    echo '</pre>';
+    // Check if the order is completed and paid
+    if ($order->has_status('completed') && $order->is_paid()) {
+        update_user_id();
+        // Payment was successful, process the order
+        // You can add your custom code here to handle a successful payment
+        // For example, update order status, send confirmation emails, etc.
+    } else {
+        // Payment was not successful
+        // You can handle unsuccessful payments here
+    }
+}
