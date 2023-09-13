3216ba54a54202d624cf598f320b817571ea9d57
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-09-13 07:48:52 -0300

    update on thank you page, need to update when payment is done

--- consultoria_presencial.php
+++ consultoria_presencial.php
@@ -17,29 +17,32 @@ $GLOBALS['current_booking_quantity'] = get_user_meta(get_current_user_id(), 'age
 
 require_once plugin_dir_path( __FILE__ ). 'src/add_my_account_tab.php';
 
-//remover esse action depois
-#add_action('woocommerce_before_checkout_form', 'update_user_booking_quantity');
 function update_user_booking_quantity($booking_quantity = 1){
     $user_id = get_current_user_id(); 
     $meta_key = 'agendamentos-presenciais-disponiveis'; 
-    if(is_int($booking_quantity)){
-        $old_meta_value = get_user_meta($user_id, $meta_key, true);
-        $new_meta_value = strval(intval($old_meta_value) + $booking_quantity);
+    $old_meta_value = get_user_meta($user_id, $meta_key, true);
+    $new_meta_value = strval(intval($old_meta_value) + intval($booking_quantity));
 
-        update_user_meta($user_id, $meta_key, $new_meta_value);
-    }
+    update_user_meta($user_id, $meta_key, $new_meta_value);
 }
 require_once plugin_dir_path(__FILE__) . 'src/custom_checkout_fields.php';
 
-add_action('woocommerce_payment_complete', 'check_payment_status_and_update_meta');
+add_action('woocommerce_thankyou', 'check_payment_status_and_update_meta');
 function check_payment_status_and_update_meta($order_id) {
+    $order = wc_get_order( $order_id );
+    $user_id = $order->get_user_id();
+    if ( $order->get_customer_ip_address() ) {  
+        $current_booking = get_post_meta( $order->get_id(), '_booking_quantity', true );
+        update_user_booking_quantity($current_booking);
+    }
+    /*
     $order = wc_get_order($order_id);
     if ($order->has_status('completed') && $order->is_paid()) {
         update_user_booking_quantity($GLOBALS['current_booking_quantity']);
         echo '<p id="booking_quantity"> adicionado </p>';
     } else {
 
-    }
+    }*/
 }
 
 add_action('AmeliaBookingAddedBeforeNotify', 'update_booking_quantity_when_book');
